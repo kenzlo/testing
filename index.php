40ca26187c81ffdb0cac850687a5816114223e3d
kenzlo <kellenfreeman@gmail.com>
2014-12-05 13:22:25 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>The Tomb of the Unknown Revolutionary War Solider</title>
+        <link href='http://fonts.googleapis.com/css?family=Lato:400,700,900' rel='stylesheet' type='text/css'>
+        <link type="text/css" rel="stylesheet" href="stylesheet.css" />
+    </head>
+    <body>
+        <div id="hmenu"> 
+		<?php include "menu.php";?>   
+        </div>        
+        <div class="center">
+            <p><h1>Tomb of the Unknown Revolutionary War Soldier</h1></p>
+            <p><img src="turws-main.jpg" width="700px"></p></div>
+            <div class="center">The links at the top allow you to access a story detailing some information about the monument itself. There is a link to images of the monument and the view information for them. Additionally there is a link to the work record for this monument. Lastly, there is a page of links relating to the monument itself. For a little bit about the project itself as well as references and credits, visit the About page.
+        </div>
+    </body>
+</html>
